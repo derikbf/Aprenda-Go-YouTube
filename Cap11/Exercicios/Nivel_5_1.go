8d96f0e9153de84c68988ab320b9db6d5e5acc49
derikbf <derikb@gmail.com>
2022-02-11 20:38:41 -0300

    Cap 11 - Nivel 5 - 1

--- Cap11/Exercicios/Nivel_5_1.go
+++ Cap11/Exercicios/Nivel_5_1.go
@@ -17,6 +17,13 @@ func main() {
 	pessoa2 := pessoa {"Frederico", "da prucia", 
 		[]string{"sabao em pó", "chocolate", "agua salgada"}}
 	
-	fmt.Println(pessoa1)
+	fmt.Println(pessoa1.nome, pessoa1.sobrenome)
+	for _, v := range pessoa1.sabores {
+		fmt.Println("\t", v)
+	}
+	
 	fmt.Println(pessoa2)
+	for _, v := range pessoa2.sabores {
+		fmt.Println("\t", v)
+	}
 }
