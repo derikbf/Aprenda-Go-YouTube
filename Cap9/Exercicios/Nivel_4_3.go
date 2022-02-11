70ea75986a318027912bebcd550f6d185bc87b60
derikbf <derikb@gmail.com>
2022-02-11 16:20:34 -0300

    Cap 9 - Exercicios - Nivel 4 - 3

---
+++ Cap9/Exercicios/Nivel_4_3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	slice := [10]int{10, 20, 30, 40, 50, 60, 70, 80, 90, 10}
+
+	fmt.Printf("%T\n", slice)
+
+	fmt.Println(slice[0:3])
+	fmt.Println(slice[4:])
+	fmt.Println(slice[1:8])
+	fmt.Println(slice[3:9])
+	
+
+}
