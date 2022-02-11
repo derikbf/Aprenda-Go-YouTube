ac094c3c9fa511fc1371252d42cb829d4e32225c
derikbf <derikb@gmail.com>
2022-02-11 16:33:45 -0300

    Cap 9 - Exercicios - Nivel 4 - 5

--- Cap9/Exercicios/Nivel_4_5.go
+++ Cap9/Exercicios/Nivel_4_5.go
@@ -4,10 +4,11 @@ import "fmt"
 
 func main() {
 	
-	x := []int{42, 43, 44, 45, 46}
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 	
-	y := []int{42, 43, 44, 48, 49, 50, 51}
-	x = append(x, y...)
+	// x = append(x[:3], x[len(x)-4:]...)
+	x = append(x[:3], x[6:]...)
+
 
 	fmt.Println(x)
 }
