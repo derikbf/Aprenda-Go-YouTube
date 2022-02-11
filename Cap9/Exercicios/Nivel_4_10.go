640857e024bef59506a65708e77aa5d2daaf2c63
derikbf <derikb@gmail.com>
2022-02-11 18:52:39 -0300

    Cap 9 - Exercicios - Nivel 4 - 10

---
+++ Cap9/Exercicios/Nivel_4_10.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	mepe := map[string][]string{
+		"dasilva_guriaestranha": []string{
+			"desaparecer", "ser assustadora", "raspar a cabeça",
+		},
+		"Senna_ayrton": []string{
+			"andar de jetski", "ser milhonario", "falarrr",
+		},
+		"pike rob": []string{
+			"Ternos loucos", "Roupas estranhas", "wtf",
+		},
+	}
+
+	mepe["loreiro_kiko"] = []string{"lavar cabelo", "quebrar quitara", "dancar"}
+	
+	delete(mepe, "dasilva_guriaestranha")
+	
+	for k, v := range mepe {
+		fmt.Println(k)
+		for i, h := range v {
+			fmt.Println("\t", i, " ", h)
+		}
+	}
+}
