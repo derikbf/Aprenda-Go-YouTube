f1eddf35cd373855bf96923100146921400bcc4e
derikbf <derikb@gmail.com>
2022-02-11 10:54:57 -0300

    Cap 7 - Exercicios - Nivel 3 - 3

---
+++ Cap7/Exercicios/Nivel_3_3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	anoemqueeunasci := 2020
+	anoateoqualeuquerocontar := 2025
+
+	for anoemqueeunasci <= anoateoqualeuquerocontar {
+		fmt.Println(anoemqueeunasci)
+		anoemqueeunasci++
+	}
+}
