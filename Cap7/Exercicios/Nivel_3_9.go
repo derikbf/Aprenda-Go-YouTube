4e84ed3d1d7414f06b71843d1c6ce5446d1c3dbc
derikbf <derikb@gmail.com>
2022-02-11 11:17:14 -0300

    Cap 7 - Exercicios - Nivel 3 - 9

---
+++ Cap7/Exercicios/Nivel_3_9.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	esportefavorito := "espeleismo"
+
+	switch esportefavorito {
+		case "futebol":
+			fmt.Println("quer jogar futebol vai pro brasil")
+		case "espeleismo":
+			fmt.Println("quer praticar ?????")
+		case "natacao":
+			fmt.Println("quer nadar vai pra piscina")
+		case "starcraft":
+			fmt.Println("quer jogar starcraft vai jogar uai")
+
+	}
+
+}
