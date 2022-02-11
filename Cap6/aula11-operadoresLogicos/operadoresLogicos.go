4ddd9ce9476683f109346dec8431fb208f5504a8
derikbf <derikb@gmail.com>
2022-02-11 10:27:23 -0300

    Cap 6 - Aula 11 - Operadores logicos

--- Cap6/aula11-operadoresLogicos/operadoresLogicos.go
+++ Cap6/aula11-operadoresLogicos/operadoresLogicos.go
@@ -5,10 +5,13 @@ import "fmt"
 
 func main() {
 
-	x := 6
+	x := 9
 
 	if (x % 2 == 0 && x % 3 == 0) {
 		fmt.Println("é multiplo de dois e treis")
 	} 
 	
+	if x%2 == 0 || x%3 == 0 {
+		fmt.Println("é multiplo de dois ou de treis")
+	}
 }
