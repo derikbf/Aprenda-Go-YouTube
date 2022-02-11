5fa38abd5da48f58799994004bb2f3d8e4482dd6
derikbf <derikb@gmail.com>
2022-02-11 12:46:07 -0300

    Cap 8 - Aula 2 - Array e Slice

---
+++ Cap8/aula2-sliceLiteral/sliceLiteral.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	array := [5]int{1, 2, 3, 4, 5}
+		fmt.Println("-- array --")	
+		fmt.Println(array)
+		fmt.Println("\n-- slice --")
+		slice := []int{1, 2, 3, 4, 5}
+		fmt.Println(slice)
+		
+}
