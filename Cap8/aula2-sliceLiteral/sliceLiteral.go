c7f65a80f6d2da3f31c7244beebda06d023d35af
derikbf <derikb@gmail.com>
2022-02-11 12:48:48 -0300

    Cap 8 - Aula 2 - Array e Slice

--- Cap8/aula2-sliceLiteral/sliceLiteral.go
+++ Cap8/aula2-sliceLiteral/sliceLiteral.go
@@ -11,5 +11,9 @@ func main() {
 		fmt.Println("\n-- slice --")
 		slice := []int{1, 2, 3, 4, 5}
 		fmt.Println(slice)
-		
+		fmt.Println(slice[3])
+
+		fmt.Println("\n-- slice 2 --")
+		slice[3] = 348756
+		fmt.Println(slice[3])
 }
