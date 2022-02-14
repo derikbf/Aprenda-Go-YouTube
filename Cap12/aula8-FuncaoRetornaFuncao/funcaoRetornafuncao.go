3267df01b5f788f31600b1632c1ca6ec5149600e
derikbf <derikb@gmail.com>
2022-02-14 18:45:58 -0300

    Cap 12 - Aula 8 - Funcoes Retornando Funcao

---
+++ Cap12/aula8-FuncaoRetornaFuncao/funcaoRetornafuncao.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	x := retornaumafuncao()
+
+	y := x(3)
+		
+	fmt.Println(y)
+
+	fmt.Println(retornaumafuncao()(3))
+}
+
+func retornaumafuncao() func(int) int {
+	return func(i int) int {
+		return i * 10
+	}
+}
