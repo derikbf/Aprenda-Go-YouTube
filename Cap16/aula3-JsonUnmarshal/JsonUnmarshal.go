2c5b6f3efafd13bb9f675cab67808df777d32fc0
derikbf <derikb@gmail.com>
2022-02-15 17:47:08 -0300

    Cap 16 - Aula 3 - Json UnMarshal

--- Cap16/aula3-JsonUnmarshal/JsonUnmarshal.go
+++ Cap16/aula3-JsonUnmarshal/JsonUnmarshal.go
@@ -2,32 +2,47 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"os"
 )
 
-
-type informacoes struct {
-	Nome string `json:"Nome"`
-	Sobrenome string `json:"Sobrenome"`
-	Idade int `json:"Idade"`
-	Profissao string `json:"Profissao"`
-	Contabancaria float64 `json:"Contabancaria"`
+type pessoa struct {
+	Nome string
+	Sobrenome string
+	Idade int
+	Profissao string
+	Contabancaria float64
 }
+
+// type informacoes struct {
+// 	Nome string `json:"Nome"`
+// 	Sobrenome string `json:"Sobrenome"`
+// 	Idade int `json:"Idade"`
+// 	Profissao string `json:"Profissao"`
+// 	Contabancaria float64 `json:"Contabancaria"`
+// }
 
 func main() {
+	jamesbond := pessoa{
+		Nome: "James",
+		Sobrenome: "Bond",
+		Idade: 40,
+		Profissao: "Agente Secreto",
+		Contabancaria: 100.5,
+	}	
 
-	sb := []byte(`{"Nome":"James","Sobrenome":"Bond","Idade":40,"Profissao":"Agente Secreto","Contabancaria":100.5}`)
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.Encode(jamesbond)
+
+	// sb := []byte(`{"Nome":"James","Sobrenome":"Bond","Idade":40,"Profissao":"Agente Secreto","Contabancaria":100.5}`)
 	
-	var jamesbond informacoes 
-	err := json.Unmarshal(sb, &jamesbond)	
-	if err != nil {
-		fmt.Println("error:", err)
-	}
+	// var jamesbond informacoes 
+	// err := json.Unmarshal(sb, &jamesbond)	
+	// if err != nil {
+	// 	fmt.Println("error:", err)
+	// }
 
-	fmt.Println(jamesbond)
-	fmt.Println(jamesbond.Profissao)
+	// fmt.Println(jamesbond)
+	// fmt.Println(jamesbond.Profissao)
 }
-
-
 
 // {"Nome":"James","Sobrenome":"Bond","Idade":40,"Profissao":"Agente Secreto","Contabancaria":100.5}
