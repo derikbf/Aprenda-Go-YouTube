700393bb53fb0fe9f40b3a682001f5ce0c0c8bfd
derikbf <derikb@gmail.com>
2022-02-15 08:56:18 -0300

    Cap 13 - Exercicios - Nivel 6 - 2

---
+++ Cap13/Exercicios/Nivel_6_2.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	si := []int{1, 2, 3, 4, 82}
+	si2 := []int{1, 2, 3, 4, 5, 6, 82}
+	
+	fmt.Println(funcao1(si...))
+	fmt.Println(funcao2(si2))
+	
+}
+
+func funcao1 (x ...int) int {
+	total := 0
+	for _, v := range x {
+		total += v
+	}
+	return total
+}
+
+func funcao2 (x []int) int {
+	total := 0
+	for _, v := range x {
+		total += v
+	}
+	return total
+}
