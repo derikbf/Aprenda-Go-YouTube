ff42ba6a960846bb91d11b40f8a0977121c44789
derikbf <derikb@gmail.com>
2022-02-15 13:47:04 -0300

    Cap 13 - Exercicios - Nivel 6 - 11

--- Cap13/Exercicios/Nivel_6_11.go
+++ Cap13/Exercicios/Nivel_6_11.go
@@ -7,3 +7,9 @@ func main() {
 	fmt.Println("Gravar Video :/")
 }
 
+// - Uma das melhores maneiras de aprender é ensinando.
+// - Para este exercício escolha o seu código favorito dentre os que vimos estudando funções. Pode ser das aulas ou da seção de exercícios. Então:
+//     - Faça download e instale isso aqui: https://obsproject.com/ 
+//     - Grave um vídeo onde *você* ensina o tópico em questão
+//     - Faça upload do vídeo no YouTube
+//     - Compartilhe o vídeo no Twitter e me marque no tweet (@ellenkorbes)
