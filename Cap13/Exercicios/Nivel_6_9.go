1a9c75f5ea548ca85fe2a1b72aeb4516c8ca3ea0
derikbf <derikb@gmail.com>
2022-02-15 13:31:53 -0300

    Cap 13 - Exercicios - Nivel 6 - 9

---
+++ Cap13/Exercicios/Nivel_6_9.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	essavaireceberaoutrafuncao(issovaiserumargumento)
+}
+
+func issovaiserumargumento() {
+	fmt.Println("Olha eu aqui")
+}
+
+func essavaireceberaoutrafuncao(x func ()) {
+	fmt.Println("Prestenção: ")
+	x()
+}
