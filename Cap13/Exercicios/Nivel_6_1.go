283a973134c1bccc307601d2a70c58f5e6270714
derikbf <derikb@gmail.com>
2022-02-15 13:51:31 -0300

    Cap 13 - Exercicios - Nivel 6 - 1

--- Cap13/Exercicios/Nivel_6_1.go
+++ Cap13/Exercicios/Nivel_6_1.go
@@ -14,3 +14,9 @@ func retornaUmInt() int {
 func retornaUmInteUmaString() (int, string) {
 	return 20, "vinte"
 }
+
+// - Exercício:
+//     - Crie uma função que retorne um int
+//     - Crie outra função que retorne um int e uma string
+//     - Chame as duas funções
+//     - Demonstre seus resultados
