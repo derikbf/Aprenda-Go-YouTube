36bacc626fde29127310b4dd0275f49ee0c6c066
derikbf <derikb@gmail.com>
2022-02-16 13:26:53 -0300

    Cap 16 - Aula 5 - Aplicacoes  - Sort

--- Cap16/aula5-CustomizandoSort/sort.go
+++ Cap16/aula5-CustomizandoSort/sort.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 
-	ss := []string{"Atirei", "o", "pau", "no", "gato"}
+	ss := []string{"A", "C", "B", "F", "D"}
 	sort.Strings(ss)
 	fmt.Println(ss)
+
+	ssi := []int{1, 4, 6, 8, 9}
+	sort.Ints(ssi)
+	fmt.Println(ssi)
 }
 
