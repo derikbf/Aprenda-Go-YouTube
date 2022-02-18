169fb073b0ce75cd706032867de98c66825e2938
derikbf <derikb@gmail.com>
2022-02-18 10:30:45 -0300

    Cap 21 - Canais - Entendendo Canais

--- Cap21/aula1-Canais/canais.go
+++ Cap21/aula1-Canais/canais.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main() {
 
-	canal := make(chan int)
+	canal := make(chan int, 2) // 1 é o buffer
 
 	go func() {
 		canal <- 42 // uma gorountine querendo entregar 
+		canal <- 43
 	}()
 
 	fmt.Println(<-canal) // uma gorountine querendo tirar
+	fmt.Println(<-canal)
 
 }
