8307d59f0c23d01e48d052416bd9d3a15b72116d
derikbf <derikb@gmail.com>
2022-02-18 07:37:57 -0300

    Cap 19 - Aula 7 - Pacotes

---
+++ Cap19/aula8-Pacotes/pkg1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"std/Aprenda-Go-YouTube/Cap19/aula8-Pacotes/pkg2"
+)
+
+func main() {
+	fmt.Println("Essa é a função main.")
+	Outro()
+	pkg2.Pkg2()
+}
---
+++ Cap19/aula8-Pacotes/pkg2/outropacote.go
@@ -0,0 +1,7 @@
+package pkg2
+
+import "fmt"
+
+func Pkg2() {
+	fmt.Println("Essa função está na Pkg2!")
+}
