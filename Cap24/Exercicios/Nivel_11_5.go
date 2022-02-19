d27d00c9ade95fc6ea652b66d66dcbe090594f1f
derikbf <derikb@gmail.com>
2022-02-19 10:04:46 -0300

    Cap 24 - Exercicios - Nivel 11 - 5

---
+++ Cap24/Exercicios/Nivel_11_5.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func TestGroupedParallel(t *testing.T) {
+// 	para _, tc := testes de intervalo {
+// 			tc := tc // captura variável de intervalo
+// 			t.Run(tc.Name, func(t *testing.T) {
+// 					t.Paralela()
+// 					...
+// 			})
+// 	}
+// }
