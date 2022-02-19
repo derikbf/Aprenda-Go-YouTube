9c01b3c081ec0df106218da6259667464ee534c0
derikbf <derikb@gmail.com>
2022-02-19 09:45:53 -0300

    Cap 24 - Exercicios - Nivel 11 - 3

--- Cap24/Exercicios/Nivel_11_2.go
+++ Cap24/Exercicios/Nivel_11_2.go
@@ -37,6 +37,5 @@ func toJSON(a interface{}) ([]byte, error) {
 	return bs, nil
 }
 
-
 // Utilizando este código: https://play.golang.org/p/9a1IAWy5E6
 // ...crie uma mensagem de erro customizada utilizando fmt.Errorf().
