10dd06478a9ee4f0687e4d11c298e617b828264f
derikbf <derikb@gmail.com>
2022-02-19 11:28:29 -0300

    Cap 27 - Aula 3 - Testes Como Exemples

---
+++ Cap27/aula3-TestesComoExemplos/main.go
@@ -0,0 +1,9 @@
+
+// Outra maneira é fazer testes como exemplos.
+// Estes exemplos são os mesmos que aparecem na documentação.
+// Para exemplos o formato é "func ExampleFuncao()"
+// Deve haver um comentário "// Output: resultado", que é o que será testado
+// Para visualizar seu exemplo na documentação, fazemos o de sempre:
+//   - godoc -http :8080
+// Tanto para testes quanto para exemplos podemos utilizar: go test ./...
+// Mais: https://blog.golang.org/examples
